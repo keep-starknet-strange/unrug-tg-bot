4644c61a6e78fc16a174bb4db2037c0c109b496c
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2024-03-13 09:31:29 +0100

    🎉 init unrug tg bot

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # unrug-tg-bot
+
 Unruggable Meme Telegram Bot
+
+# Usage
+
+```bash
+# Test with PAL token
+
+# Command:
+/unrug 0x49201f03a0f0a9e70e28dcd74cbf44931174dbe3cc4b2ff488898339959e559
+
+# Response:
+This token IS Unruggable ✅
+Token name: Paint au lait
+Token symbol: PAL
+Team alloc: 0.2%
+Marketcap: 5M
+```
---
+++ index.ts
@@ -0,0 +1,65 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const bot = getBot();
+
+console.log("Bot is running...");
+
+// ************ COMMANDS ************
+
+// /unrug command
+// Matches "/unrug [token_address]"
+bot.onText(/\/unrug (.+)/, (msg: { chat: { id: any } }, match: any[]) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const tokenAddress = match[1]; // the captured "token_address"
+
+  // Prepare the response
+  var response;
+
+  // Check if the provided address is a valid StarnNet address
+  if (!isValidStarknetAddress(tokenAddress)) {
+    response = `The provided address is a not valid Starknet address: ${tokenAddress}`;
+    console.log(response);
+    bot.sendMessage(chatId, response);
+  } else {
+    console.log(
+      `The provided address is a valid Starknet address: ${tokenAddress}`,
+    );
+  }
+});
+
+// ************ FUNCTIONS ************
+
+/**
+ * Returns a Telegram bot instance.
+ *
+ * @returns A Telegram bot instance.
+ */
+function getBot(): any {
+  // replace the value below with the Telegram token you receive from @BotFather
+  const telegramBotToken = Bun.env.TELEGRAM_BOT_TOKEN;
+
+  // Exit if the token is not provided
+  if (!telegramBotToken) {
+    console.error("TELEGRAM_BOT_TOKEN is not provided");
+    process.exit(1);
+  }
+
+  return new TelegramBot(telegramBotToken, { polling: true });
+}
+
+/**
+ * Checks if a given string is a valid StarkNet address.
+ *
+ * A valid StarkNet address must start with '0x' followed by 63 or 64 hexadecimal characters.
+ *
+ * @param address - The string to be tested against the StarkNet address format.
+ * @returns `true` if the string is a valid StarkNet address, otherwise `false`.
+ */
+function isValidStarknetAddress(address: string): boolean {
+  const regex = /^0x[0-9a-fA-F]{63,64}$/;
+  return regex.test(address);
+}
