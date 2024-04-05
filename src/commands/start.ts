173bb9db2d95ac11a160aa447659fdee9f55b24d
Uğur Eren <contact@ugureren.net>
2024-04-05 15:02:09 +0300

    Fix start command

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,6 +1,6 @@
 import { bot } from '../services/bot'
 
-bot.onText(/\/start (.+)/, async (msg): Promise<void> => {
+bot.onText(/\/start/, async (msg): Promise<void> => {
   await bot.sendMessage(
     msg.chat.id,
     `Hello, You can use /unrug command to check if a token is unrugabble or not.`,
